00397e390d999738e480ed369b1ff60aa541865a
DDadona <doug.dadona@gmail.com>
2026-01-04 15:44:07 -0400

    add files database.js and .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(req, res) {
-  res.status(200).json({ status: 'Acima da média' });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result  = await database.query("SELECT 1 + 1 as numero;");
+  console.log(result.rows);
+  response.status(200).json({ status: 'Acima da média' });
 }
 
 export default status;
